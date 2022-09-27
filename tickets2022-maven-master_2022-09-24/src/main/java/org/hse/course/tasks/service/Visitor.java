f1d913874677360241cb4bf0ff8d4ddab6996af0
Rsprox <11vsrsx11@gmail.com>
2022-09-27 19:44:42 +0500

    добавлен посетитель для проверки на нечётность

--- tickets2022-maven-master_2022-09-24/src/main/java/org/hse/course/tasks/service/Visitor.java
+++ tickets2022-maven-master_2022-09-24/src/main/java/org/hse/course/tasks/service/Visitor.java
@@ -8,7 +8,6 @@ import org.hse.course.tasks.data.Ticket;
  * @param <R> тип результата работы метода {@link Visitor#visit(Object)}
  */
 public interface Visitor<T, R> {
-    //todo добавить посетителя, проверяющего кратность пяти + фабрику таких посетитиелей
     static Factory<Ticket, Boolean> getEvenVisitorFactory() {
         return new EvenTicketVisitorFactory();
     }
@@ -34,5 +33,23 @@ class EvenTicketVisitorFactory implements Visitor.Factory<Ticket, Boolean> {
             var modulo = item.getNumber() % 2;
             return modulo == 0;
         }
+    }
+}
+
+class OddTicketVisitorFactory implements Visitor.Factory<Ticket, Boolean> {
+
+    @Override
+    public Visitor<Ticket, Boolean> getInstance() {
+        return new OddTicketVisitorFactory.OddTicketVisitorImpl();
+    }
+
+    private class OddTicketVisitorImpl implements Visitor<Ticket, Boolean> {
+
+        @Override
+        public Boolean visit(Ticket item) {
+            var modulo = item.getNumber() % 2;
+            return modulo == 1;
+        }
+
     }
 }
